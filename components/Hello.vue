7805baf60770d6cc98f1c33f3dadbd113deb95ed
Jack Scott <jack.scott10@gmail.com>
2020-02-03 21:56:20 +0000

    Add hello text area

---
+++ components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h3>HELLO.</h3>
+    <p>The text for hello will go here.</p>
+    <!-- <h2>{{hello.title}}</h2>
+    <div v-html="hello.description" class="blog__body"></div> -->
+    <ul class="list">
+      <li>github</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData() {
+    try {
+      const hello = await import('~/content/data/hello.json');
+      // const hello = {
+      //   title: "I'm Jack Scott",
+      //   description:
+      //     "👋 I'm a software engineer at Play Sports Network solving technical and product solutions.\n\nI’m always attempting to expand my knowledge."
+      // };
+      return {
+        hello
+      };
+    } catch (err) {
+      console.debug(err);
+      return false;
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,40 @@
 // the posts are passed down to the Blog List component to be rendered
 
 <template>
-  <div>
-    <BlogList :posts="posts" />
+  <div class="layout">
+    <section>
+      <Hello />
+    </section>
+    <section>
+      <BlogList :posts="posts" />
+    </section>
   </div>
 </template>
 
 <script>
-import BlogList from '~/components/BlogList.vue'
+import BlogList from '~/components/BlogList.vue';
+import Hello from '~/components/Hello.vue';
 
 export default {
   layout: 'layout',
   components: {
+    Hello,
     BlogList
   },
   async asyncData() {
-      // create context via webpack to map over all blog posts
-      const allPosts = await require.context("~/content/blog-posts/", true, /\.md$/)
-      const posts =  allPosts.keys().map((key) => {
-        // give back the value of each post context
-        return allPosts(key)
-      });
-      return {
-        posts
-      }
+    // create context via webpack to map over all blog posts
+    const allPosts = await require.context(
+      '~/content/blog-posts/',
+      true,
+      /\.md$/
+    );
+    const posts = allPosts.keys().map((key) => {
+      // give back the value of each post context
+      return allPosts(key);
+    });
+    return {
+      posts
+    };
   }
-}
+};
 </script>
